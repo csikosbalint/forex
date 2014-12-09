70cb87a3601bb98cf19401b76b9fb68285118b16
Balint Csikos <csikos.balint@fnf.hu>
2014-12-09 22:22:12 +0100

    trying kar assemble

--- platform/src/main/java/hu/fnf/devel/forex/platform/PlatformActivator.java
+++ platform/src/main/java/hu/fnf/devel/forex/platform/PlatformActivator.java
@@ -1,11 +1,4 @@
-package hu.fnf.devel.forex.platform;//import com.dukascopy.api.Instrument;
-
-import com.dukascopy.api.system.ClientFactory;
-import com.dukascopy.api.system.IClient;
-import com.dukascopy.api.system.ISystemListener;
-import org.apache.log4j.Logger;
-
-import java.util.Properties;
+package hu.fnf.devel.forex.platform;
 
 /**
  * Created by johnnym on 08/12/14.
