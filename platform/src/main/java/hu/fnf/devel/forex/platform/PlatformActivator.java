7c0ccdb3185e8e44e4a92a46e8ec4d483243e7ac
csikosbalint <csikos.balint@fnf.hu>
2014-12-10 12:59:54 +0100

    test

--- platform/src/main/java/hu/fnf/devel/forex/platform/PlatformActivator.java
+++ platform/src/main/java/hu/fnf/devel/forex/platform/PlatformActivator.java
@@ -1,8 +1,12 @@
 package hu.fnf.devel.forex.platform;
 
+import com.dukascopy.api.system.IClient;
+
 /**
  * Created by johnnym on 08/12/14.
  */
 public class PlatformActivator {
+        public IClient iClient;
+
 
 }
